0164d6d9f43a8ea3bfa88ff1cccb813a934e3993
Felipe Pereira <felipe.lfpm@outlook.com>
2020-10-26 10:34:28 -0300

    Lista de PETs no perfil do usuário

--- app/Views/site/paginas/perfil_content/perfil_listarPets.php
+++ app/Views/site/paginas/perfil_content/perfil_listarPets.php
@@ -1,5 +1,94 @@
+<!-- Vou criar o CSS aqui e depois a gente vê onde coloca -->
+<style type="text/css">
+
+	i {
+		margin-right: 5px;
+	}
+
+	.pet-item {
+		flex-direction: row !important;
+	}
+
+	.pet-pic {
+		width: 300px;
+	}
+
+	@media only screen and (max-width: 768px) {
+		.pet-item {
+			flex-direction: column !important;
+		}
+
+		.pet-pic {
+			margin: 0 auto;
+		}
+	}
+
+</style>
+
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+
 <div class="tab-pane" id="listarPetsCadastrados">
 <form>
-    Aqui listagem
+    <div class="container">
+    	<div class="row">
+    		<div class="col-12 col-lg-6">
+    			<div class="card pet-item">
+					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+					<div class="card-body text-left">
+						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+						<hr>
+						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+					</div>
+				</div>
+    		</div>
+    		<div class="col-12 col-lg-6">
+    			<div class="card pet-item">
+					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+					<div class="card-body text-left">
+						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+						<hr>
+						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+					</div>
+				</div>
+    		</div>
+    		<div class="col-12 col-lg-6">
+    			<div class="card pet-item">
+					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+					<div class="card-body text-left">
+						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+						<hr>
+						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+					</div>
+				</div>
+    		</div>
+    		<div class="col-12 col-lg-6">
+    			<div class="card pet-item">
+					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+					<div class="card-body text-left">
+						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+						<hr>
+						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+					</div>
+				</div>
+    		</div>
+    	</div>
+    </div>
 </form>
 </div>
