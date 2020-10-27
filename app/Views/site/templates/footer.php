47042f8e2d75ee7fd251e51db855c52011491b67
luckvice <lucasmarcelo93@gmail.com>
2020-10-27 11:09:31 -0300

    Melhorias js dinamicos

--- app/Views/site/templates/footer.php
+++ app/Views/site/templates/footer.php
@@ -34,16 +34,16 @@
     </div>
   </footer>
   <!--   Core JS Files   -->
-  <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="../assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-  <script src="../assets/js/plugins/moment.min.js"></script>
+  <script src="<?= base_url('assets/js/core');?>/jquery.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets/js/core');?>/popper.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets/js/core');?>/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets/js');?>/plugins/moment.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/js/bootstrap-select.min.js"></script>
   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets/js');?>/plugins/nouislider.min.js" type="text/javascript"></script>
   <!--  Google Maps Plugin    -->
   <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/material-kit.js?v=2.0.7" type="text/javascript"></script>
+  <script src="<?= base_url('assets/js/');?>/material-kit.js?v=2.0.7" type="text/javascript"></script>
 <script>
 $(function(){
   var hash = window.location.hash;
