0e9221207288d04378157f0b56e986710dcafff3
Felipe Pereira <felipe.lfpm@outlook.com>
2020-10-27 03:01:56 -0300

    Adição de botões na lista de pets

--- app/Views/site/paginas/perfil_content/perfil_listarPets.php
+++ app/Views/site/paginas/perfil_content/perfil_listarPets.php
@@ -8,11 +8,17 @@
 	}
 
 	.pet-item {
+		display: flex;
 		flex-direction: row !important;
 	}
 
 	.pet-pic {
-		width: 300px;
+		width: 250px;
+	}
+
+	.pet-buttons {
+		margin-bottom: 1rem;
+		text-align: center;
 	}
 
 	@media only screen and (max-width: 768px) {
@@ -35,58 +41,90 @@
     <div class="container">
     	<div class="row">
     		<div class="col-12 col-lg-6">
-    			<div class="card pet-item">
-					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
-					<div class="card-body text-left">
-						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
-						<hr>
-						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
-						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
-						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
-						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
-						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+    			<div class="card">
+    				<div class="pet-item">
+						<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+						<div class="card-body text-left">
+							<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+							<hr>
+							<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+							<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+							<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+							<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+							<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+						</div>
+					</div>
+					<hr>
+					<div class="pet-buttons">
+						<button type="button" class="btn btn-primary">Ver</button>
+						<button type="button" class="btn btn-success">Adotado</button>
+						<button type="button" class="btn btn-danger">Deletar</button>
 					</div>
 				</div>
     		</div>
     		<div class="col-12 col-lg-6">
-    			<div class="card pet-item">
-					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
-					<div class="card-body text-left">
-						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
-						<hr>
-						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
-						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
-						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
-						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
-						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+    			<div class="card">
+    				<div class="pet-item">
+						<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+						<div class="card-body text-left">
+							<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+							<hr>
+							<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+							<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+							<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+							<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+							<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+						</div>
+					</div>
+					<hr>
+					<div class="pet-buttons">
+						<button type="button" class="btn btn-primary">Ver</button>
+						<button type="button" class="btn btn-success">Adotado</button>
+						<button type="button" class="btn btn-danger">Deletar</button>
 					</div>
 				</div>
     		</div>
     		<div class="col-12 col-lg-6">
-    			<div class="card pet-item">
-					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
-					<div class="card-body text-left">
-						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
-						<hr>
-						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
-						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
-						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
-						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
-						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+    			<div class="card">
+    				<div class="pet-item">
+						<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+						<div class="card-body text-left">
+							<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+							<hr>
+							<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+							<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+							<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+							<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+							<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+						</div>
+					</div>
+					<hr>
+					<div class="pet-buttons">
+						<button type="button" class="btn btn-primary">Ver</button>
+						<button type="button" class="btn btn-success">Adotado</button>
+						<button type="button" class="btn btn-danger">Deletar</button>
 					</div>
 				</div>
     		</div>
     		<div class="col-12 col-lg-6">
-    			<div class="card pet-item">
-					<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
-					<div class="card-body text-left">
-						<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
-						<hr>
-						<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
-						<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
-						<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
-						<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
-						<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+    			<div class="card">
+    				<div class="pet-item">
+						<img class="card-img-top pet-pic" src="https://www.specialdog.com.br/assets/imgs/cao.png" alt="Card image cap">
+						<div class="card-body text-left">
+							<h5 class="card-title"><i class="fas fa-dog"></i>Nico</h5>
+							<hr>
+							<p class="card-text"><i class="fas fa-mars"></i>Macho</p>
+							<p class="card-text"><i class="fas fa-paw"></i>Dócil</p>
+							<p class="card-text"><i class="fas fa-dumbbell"></i>Pequeno</p>
+							<p class="card-text"><i class="fas fa-history"></i>Jovem</p>
+							<p class="card-text"><i class="fas fa-map-marker-alt"></i>Porto Alegre/RS</p>
+						</div>
+					</div>
+					<hr>
+					<div class="pet-buttons">
+						<button type="button" class="btn btn-primary">Ver</button>
+						<button type="button" class="btn btn-success">Adotado</button>
+						<button type="button" class="btn btn-danger">Deletar</button>
 					</div>
 				</div>
     		</div>
