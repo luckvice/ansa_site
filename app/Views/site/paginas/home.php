207060aa461ebb058a50a8f80f11d896dd66a612
Felipe Pereira <felipe.lfpm@gmail.com>
2020-11-10 02:39:40 -0300

    Tela inicial: Botoes de filtragem por PET

--- app/Views/site/paginas/home.php
+++ app/Views/site/paginas/home.php
@@ -8,12 +8,19 @@
     <div class="container">
       <div class="row">
         <div class="col-md-8 ml-auto mr-auto text-center">
-          <h1 class="title"> Encontre um amigo</h1>
-          <h4>Essa é a chance de encontrar muitos amigos que estão a sua espera, use os filtros abaixo se quiser
-            enontrar aquele amigo(a)
-            especial!</h4>
+          <h1 class="title">Que tipo de <span style="color: #d6a7ff;">amigo</span> está procurando?</h1>
         </div>
-        <?= $this->include('site/paginas/home_content/destaque_filtro') ?>
+      </div>
+      <div class="row">
+        <div class="col-md-12 ml-auto mr-auto text-center">
+          <img class="home-pet-button" src="<?=base_url('assets/img/dog.png')?>" alt="Botão de Cachorro" data-toggle="tooltip" data-placement="top" title="Cachorrinho">
+          <img class="home-pet-button" src="<?=base_url('assets/img/cat.png')?>" alt="Botão de Gato" data-toggle="tooltip" data-placement="top" title="Gatinho">
+        </div>
+      </div>
+      <div class="row" style="margin-top: 20px;">
+        <div class="col-md-8 ml-auto mr-auto text-center">
+          <h5 class="font-weight-bold">Essa é a chance de encontrar muitos amigos que estão à sua espera! Clique em uma das opções e procure pelo seu novo parceiro!</h5>
+        </div>
       </div>
     </div>
   </div>
