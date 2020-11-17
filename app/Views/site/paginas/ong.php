49ba7eba1812cb3eabea764570e208c7226ab4c0
Felipe Pereira <felipe.lfpm@outlook.com>
2020-11-17 20:28:14 -0300

    Tela da Ong

--- app/Views/site/paginas/ong.php
+++ app/Views/site/paginas/ong.php
@@ -1,9 +1,7 @@
 <?= $this->extend('site/templates/base_template') ?>
-
 <?= $this->section('content') ?>
 
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-
 <div class="page-header pet-cover" data-parallax="true"></div>
 <div class="main main-raised">
 	<div class="profile-content">
@@ -21,7 +19,6 @@
                     </div>
 	            </div>
             </div>
-            
 			<div class="row">
 				<div class="col-md-6 ml-auto mr-auto">
 					<div class="card">
@@ -31,21 +28,16 @@
 									<h5 class="card-title"><i class="fas fa-share-alt"></i> <span style="color: purple;">Contato</span</h5>
 								</div>
 								<hr>
-
 								<?php if(count($campos_contato)) : ?>
 									
 									<?php foreach ($campos_contato as $campo) { ?>
 										<p class="card-text"><a class="external-link" href="<?=$ong->$campo?>" target="_blank"><i class="fas fa-desktop"></i> <?=$ong->$campo?></a></p><?php
 									}
-
 								?>
-
 								<?php else : ?>
 
 									<p class="card-text"><i class="fas fa-times"></i> Não há informações sobre o contato dessa ONG</p>
-
 								<?php endif; ?>
-
 							</div>
 						</div>
 					</div>
@@ -76,7 +68,7 @@
 
     		<div class="row">
 				
-				<?php foreach($listaPets as $key => $pet) { ?>
+				<?php foreach($listaPets as $key => $pet){ ?>
 
 				<div class="col-12 col-md-4">
 					<div class="card">
