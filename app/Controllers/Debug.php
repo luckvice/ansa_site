1c432e4df73ec14a0b107d50de6e7e4bbea1597d
luckvice <lucasmarcelo93@gmail.com>
2020-11-23 21:56:37 -0300

    Refatoracao Recomendador

--- app/Controllers/Debug.php
+++ app/Controllers/Debug.php
@@ -9,13 +9,16 @@ use App\Models\Usuarios; //Carrega Model SQL
 use App\Models\Cidades; //Carrega Model SQL
 use App\Models\Estados; //Carrega Model SQL
 use App\Models\Pets; //Carrega Model SQL
-use App\Models\Recomendacao;
+use App\Libraries\Recomendar;
 use App\Libraries\Sima;
 use App\Helpers\Geopets;
 
 class Debug extends Controller
 {
-
+    public function recomendar(){
+        $recomendar = new recomendar;
+        $recomendar->recomendar();
+    }
     public function removerrecomendacaotest(){
         $recomenda = new Recomendacao;
     
@@ -275,60 +278,5 @@ class Debug extends Controller
 
     }
 
-    function recomendar() {
 
-        $recomendacao = new Recomendacao;
-        $pets = new Pets;
-        $mensagem = new Sima;
-
-        $desejos = $recomendacao->listarRecomendacoes();
-
-        $consultas_realizadas = array();
-        $recomendacoes_para_excluir = array();
-
-        foreach ($desejos as $key => $desejo) {
-            
-            $caracteristicas_hash = md5($desejo->id_cidade . $desejo->id_porte . $desejo->id_sexo . $desejo->id_especie);
-
-            if (!array_key_exists($caracteristicas_hash, $consultas_realizadas)) :
-                $consulta = $pets->getPetsByRecomendacao($desejo);
-                $consultas_realizadas[$caracteristicas_hash] = $consulta;
-            else :
-                $consulta = $consultas_realizadas[$caracteristicas_hash];
-            endif;
-
-            // Caso haja PETs correspondentes ao interesse do solicitante:
-            if(count($consulta) > 0) {
-                
-                //Montamos o link de acesso para a consulta dos pets
-                $data['link'] = base_url('/') . '/pets/todos/0/' . $desejo->id_porte . '/' . $desejo->id_sexo . '/0/' . $desejo->id_cidade;
-                
-                // Montamos o texto que será enviado para o WhatsApp do interessado
-                $texto = "Oie! 😊💜
-                
-🙋‍♂️ Venho aqui para te informar que um novo amiguinho que você procurava chegou no Amigo Não se Abandona! 🐶😸
-                
-◾ Estamos enviando essa recomendação de acordo com todas as características as quais você estava procurando! 😻
-                
-◾ Acesse o link e adote seu novo amigo! 💜 " . $data['link'];
-                
-                if($desejo->telefone)
-                    $mensagem->enviarMensagemWa($desejo->telefone, $texto);
-                
-                $data['img_header'] = base_url('assets/img/dog_perfil.jpg');
-                
-                if($desejo->email)
-                    $mensagem->enviarMensagemEmail('Sugestão de Adoção | Amigo Não se Abandona', view('site/templates/email/sugestao_adocao',$data), $desejo->email);
-
-                // Como a recomendação foi enviada, adicionamos a lista de exclusão
-                $recomendacoes_para_excluir[] = $desejo->id_disparo_recomendacao;
-                
-            }
-
-        }
-
-        // Excluímos as recomendações enviadas
-        $recomendacao->limpar($recomendacoes_para_excluir);
-
-    }
 }
